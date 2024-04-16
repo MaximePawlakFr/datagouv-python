b56efb2d68dc5cdd657653447c09c2ec4484e76e
Maxime Pawlak <contact@maximepawlak.fr>
2024-04-16 21:39:25 +0200

    Add test for ResourcesDownloader

--- datagouv/downloader/_resources_downloader.py
+++ datagouv/downloader/_resources_downloader.py
@@ -1,29 +1,55 @@
+from __future__ import annotations
 from datagouv import DatagouvClient
 import datagouv.downloader._utils as _utils
 
 
-class ResourcesDownloader(object):
-    def __init__(self, dataset_id, DatagouvClient=DatagouvClient):
-        self.dataset_id = dataset_id
+RESOURCE_TYPES = ["main", "documentation", "update", "api", "code", "other"]
 
-        self.dataset = None
-        self.resources = []
-        self.urls = []
+
+class ResourcesDownloader(object):
+    def __init__(
+        self,
+        dataset_id: str,
+        resource_types: str | list[str] = "all",
+        DatagouvClient=DatagouvClient,
+    ) -> None:
+        """
+        resource_types: ["main", "documentation", "update", "api", "code", "other"]
+        """
+        self.dataset_id: str = dataset_id
+
+        self.dataset: list = None
+        self.resources: list = []
+        self.urls: list = []
+
+        if resource_types == "all":
+            self.resource_types: list = RESOURCE_TYPES
+        elif isinstance(resource_types, list):
+            self.resource_types = resource_types
 
         self.client = DatagouvClient()
 
         self.prepare()
 
-    def prepare(self):
+    def prepare(self) -> None:
         """
         Fetch dataset, set resources and urls.
         """
         self.dataset = self.client.datasets.get(self.dataset_id)
         self.resources = self.dataset.get("resources")
-        self.urls = [resource.get("url") for resource in self.resources]
+        self.urls = []
+
+        for resource in self.resources:
+            # Add url if type is allowed
+            type = resource.get("type")
+            print(self.resource_types, type, type in self.resource_types)
+            if type in self.resource_types:
+                url = resource.get("url")
+                self.urls.append(url)
+
         return None
 
-    def download(self, directory_path: str = None):
+    def download(self, directory_path: str = None) -> list:
         """
         Download resources into `directory_path`
         """
--- datagouv/downloader/_resources_downloader__test.py
+++ datagouv/downloader/_resources_downloader__test.py
@@ -1,8 +1,18 @@
-from datagouv import ResourcesDownloader
 import pytest
 
+from datagouv import ResourcesDownloader
+from datagouv.downloader._resources_downloader import RESOURCE_TYPES
+
+resource_1 = {"id": "r_1_id", "url": "r_1_url", "type": "main"}
+resource_2 = {"id": "r_2_id", "url": "r_2_url", "type": "main"}
+resource_3 = {"id": "r_3_id", "url": "r_3_url", "type": "documentation"}
+resource_4 = {"id": "r_4_id", "url": "r_4_url", "type": "other"}
+
 dataset_1_id = "id_d_1"
-dataset_1 = {"id": "dataset_1_id", "resources": [{"id": "r_1_id", "url": "r_1_url"}]}
+dataset_1 = {
+    "id": "dataset_1_id",
+    "resources": [resource_1, resource_2, resource_3, resource_4],
+}
 
 
 class MockDatasetsService(object):
@@ -22,7 +32,13 @@ def test_default():
     assert d is not None
     assert d.dataset_id == dataset_1_id
 
-    # Test resrouce
+    # By default, all files types
+    assert d.resource_types == RESOURCE_TYPES
+
+    # Test resources
+    assert len(d.resources) == 4
+
+    # Test resource
     resource = d.resources[0]
     assert resource.get("id") == "r_1_id"
     assert resource.get("url") == "r_1_url"
@@ -32,4 +48,26 @@ def test_no_dataset_id():
     with pytest.raises(TypeError) as error:
         ResourcesDownloader(DatagouvClient=MockDatagouvClient)
     print(error)
-    assert str(error.value) == "__init__() missing 1 required positional argument: 'dataset_id'"
+    assert (
+        str(error.value)
+        == "__init__() missing 1 required positional argument: 'dataset_id'"
+    )
+
+
+def test_only_main_files():
+    d = ResourcesDownloader(dataset_1_id, ["main"], DatagouvClient=MockDatagouvClient)
+
+    # Test files types
+    assert d.resource_types == ["main"]
+    assert len(d.resources) == 4
+    assert len(d.urls) == 2
+
+
+def test_main_and_doc_files():
+    d = ResourcesDownloader(
+        dataset_1_id, ["main", "documentation"], DatagouvClient=MockDatagouvClient
+    )
+    # Test files types
+    assert d.resource_types == ["main", "documentation"]
+    assert len(d.resources) == 4
+    assert len(d.urls) == 3
